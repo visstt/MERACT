854f7fe070490d7d601ca99c1925ae8a937f1d01
visstt <egorskomorohov020606@gmail.com>
2025-09-02 15:47:16 +0500

    feat: Update index title to 'MERACT' and enhance GuildCard component with layout and styling improvements

--- src/pages/guilds/components/GuildCard.jsx
+++ src/pages/guilds/components/GuildCard.jsx
@@ -1,5 +1,27 @@
 import styles from "./GuildCard.module.css";
 
 export default function GuildCard() {
-  return <div className={styles.guildCard}></div>;
+  return (
+    <div className={styles.guildCard}>
+      <div className={styles.content}>
+        <img
+          src="/icons/guild/light.svg"
+          alt="light"
+          className={styles.light}
+        />
+        <div className={styles.name}>
+          <h1>Guild Name</h1>
+        </div>
+        <div className={styles.description}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex illo
+            fugiat reiciendis culpa
+          </p>
+        </div>
+        <div className={styles.info}></div>
+        <p>Guild Acts</p>
+        <h2>145/179</h2>
+      </div>
+    </div>
+  );
 }
